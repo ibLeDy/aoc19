04053ddb985510850cdc1a9d5e09ab145128cea4
bLeDy <deejaynof@gmail.com>
2019-12-13 16:54:02 +0100

    Name formatting v2

--- day-01/part_one.py
+++
@@ -1,9 +0,0 @@
-with open("input.txt", "r") as f:
-    modules = f.readlines()
-
-required_fuel = 0
-for module in modules:
-    module_fuel = int(module.strip("\n")) // 3 - 2
-    required_fuel += module_fuel
-
-print(required_fuel)
